86bb39f204a42238bef3fab492e646354998da7b
Sean McGillis <slcidevice@yahoo.com>
2023-06-07 23:24:20 -0600

    Added some stuff to try and get the api endpoints to work. They didn't

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    function add() {
+        return ["result"=>"add worked!"];
+    }
+    function try() {
+        return ["result"=>"try worked!"];
+    }
 }
