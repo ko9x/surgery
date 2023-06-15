ae5510218a27e1e511f9dbfbb2ad4ea8123a105c
Sean McGillis <slcidevice@yahoo.com>
2023-06-15 16:28:45 -0600

    Created the function and route to store a range

--- src/app/Http/Controllers/RangeController.php
+++ src/app/Http/Controllers/RangeController.php
@@ -3,9 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Range;
 
 class RangeController extends Controller
 {
+    function store(Request $request, $item_id) {
+        $data = $request->all();
+
+        $range = Range::create([
+            'item_id' => $item_id,
+            'name' => $data['name'],
+            'starts_at' => $data['starts_at'],
+            'ends_at' => $data['ends_at'],
+            'details' => $data['details']
+        ]);
+
+        if (!$range) {
+            return response()->json(['success' => true, 'error' => 'range not created' ]);
+        } else {
+            return response()->json(['success' => true, 'range' => $range ]);
+        }
+    }
+
+    // Can probably delete this. I can't think of a reason to show this without including the item it is associated with
+    // function show($id) {
+    //     $range = Range::find($id);
+
+    //     if (!$range) {
+    //         return response()->json(['success' => true, 'error' => 'range not found' ]);
+    //     } else {
+    //         return response()->json(['success' => true, 'range' => $range ]);
+    //     }
+
+    // }
+
     function add() {
         return ["result"=>"add range worked!"];
     }
--- src/routes/api.php
+++ src/routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// ItemController routes
 Route::post("item", [ItemController::class, 'store']);
 
 Route::post("item/add",[ItemController::class, 'add']);
@@ -28,7 +29,12 @@ Route::get("item/try",[ItemController::class, 'try']);
 
 Route::get("item/{id}", [ItemController::class, 'show']);
 
+// RangeController routes
+Route::post("item/{item_id}/range", [RangeController::class, 'store']);
+
 Route::post("range/add",[RangeController::class, 'add']);
 
 Route::get("range/try",[RangeController::class, 'try']);
+
+Route::get("item/{id}", [RangeController::class, 'show']);
 
