bfc2ea0e7eff7a9b624dc9c9bf7ed8f2e6375b05
Sean McGillis <slcidevice@yahoo.com>
2023-08-02 15:17:04 -0600

    Only show the character length alert if the user tries to add a name that is too short. If the user hits cancel, no alert is shown.

--- src/public/js/manageItems.js
+++ src/public/js/manageItems.js
@@ -41,6 +41,9 @@ for (var i = 0; i < bottomResultContainer.length; i++) {
                 editItemButton.addEventListener('click', (e) => {
                     let itemId = e.target.parentNode.parentNode.id;
                     let newName = prompt("Please enter a new name for the item");
+                    if(newName === null) {
+                        return;
+                    }
                     if(newName && newName.length > 7) {
                         editItem(itemId, {name: newName});
                     } else {
